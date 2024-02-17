48c9203f0b3b222952c9bed280dae2dc1be127dc
Gilvan-R-A <gilvan.dev@gmail.com>
2024-02-17 17:35:26 -0300

    Solução do exercício 01 da lista de Estrutura de Decisão

---
+++ EstruturaDeDecisao/Exercicio01.js
@@ -0,0 +1,7 @@
+// Faça um Programa que peça dois números e imprima o maior deles.
+
+let num1 = Number(prompt("Digite o primeiro valor: "));
+let num2 = Number(prompt("Digite o segundo valor: "));
+let soma = num1 + num2;
+
+alert("Soma: " + soma);
