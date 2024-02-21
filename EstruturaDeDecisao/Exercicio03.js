b6e4934084cebafc3defe254baa4729ff36d4ee9
Gilvan-R-A <gilvan.dev@gmail.com>
2024-02-21 10:31:08 -0300

    Solução do Exercício 03 da lista de Estrutura de Decisão

---
+++ EstruturaDeDecisao/Exercicio03.js
@@ -0,0 +1,11 @@
+// Faça um Programa que verifique se uma letra digitada é "F" ou "M". Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
+
+let letra = prompt('Digite "F" para feminino ou "M" para masculino');
+
+if(letra === 'F' || letra === 'f'){
+    alert("Feminino");
+}else if(letra === 'M' || letra === 'm'){
+    alert("Masculino");
+}else{
+    alert("Sexo inválido");
+}
