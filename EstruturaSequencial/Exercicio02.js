b62a33ccf0df213d0a88491f5dc98ce9a292de7f
Gilvan-R-A <gilvan.dev@gmail.com>
2024-03-04 16:33:53 -0300

    Solução do Exercício 02 da lista de Estrutura Sequencial

---
+++ EstruturaSequencial/Exercicio02.js
@@ -0,0 +1,4 @@
+// Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+
+let number = Number(prompt("Digite um número: "));
+alert(`O número informado foi ${number}`);
