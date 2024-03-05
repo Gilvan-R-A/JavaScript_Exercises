7438f85fb87c47330ee59e8ab86b8ae5751f8760
Gilvan-R-A <gilvan.dev@gmail.com>
2024-03-05 18:06:13 -0300

    Solução do Exercício 03 da lista Estrutura Sequencial

---
+++ EstruturaSequencial/Exercicio03.js
@@ -0,0 +1,7 @@
+// Faça um Programa que peça dois números e imprima a soma.
+
+let num1 = Number(prompt("Digite o 1º número: "));
+let num2 = Number(prompt("Digite o 2º número: "));
+let soma = num1 + num2;
+
+alert(`Soma: ${soma}`);
